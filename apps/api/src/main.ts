9125f54b3294de4138621b9aacda1c515c9f1df2
Reuben Agbor <reubenagbor10@gmail.com>
2026-03-19 16:31:51 +0100

    feat(api): initialize NestJS application with basic structure and configurations

---
+++ apps/api/src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Theokallia API')
+    .setDescription('Jewellery store API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.PORT ?? 3333);
+}
+bootstrap();
