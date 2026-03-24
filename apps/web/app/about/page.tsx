b66816ff77c61eb1251c7fbb783d3b16684385e7
Reuben Agbor <reubenagbor10@gmail.com>
2026-03-24 14:32:25 +0100

    about and contact

--- apps/web/app/about/page.tsx
+++ apps/web/app/about/page.tsx
@@ -1,8 +1,52 @@
+import Features from '@/components/homepage/features'
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className="w-full">
+      <div className="flex items-center gap-16 bg-neutral-25 px-30 py-20">
+        <div className="w-[380px] shrink-0">
+          <Image
+            src="/images/necklace-3.webp"
+            alt="About Theokallia"
+            width={380}
+            height={480}
+            className="h-full w-full object-cover"
+          />
+        </div>
+
+        <div className="mt-4 flex max-w-2xl flex-col space-y-10">
+          <h1 className="font-le-jour text-6xl tracking-tight uppercase">
+            About Us
+          </h1>
+          <p className="font-sans text-2xl/8 font-light text-black">
+            At Theokallia, we believe jewelry is more than an accessory . it's a
+            reflection of identity, elegance, and timeless beauty. Every piece
+            is thoughtfully designed to elevate your everyday style while
+            celebrating life's most meaningful moments.
+          </p>
+
+          <div>
+            <Button
+              asChild
+              className="px-6 py-4 tracking-wide transition-colors"
+            >
+              <Link href="/shop">Shop</Link>
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      <div className='flex flex-col items-center justify-center pt-10'>
+        <h2 className="font-sans text-2xl font-light tracking-wide">
+          Why choose us
+        </h2>
+        <Features />
+      </div>
+    </div>
   )
 }
 
