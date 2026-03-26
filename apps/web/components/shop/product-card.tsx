f1be36bb83333abb3b5e1f7f889e39f8b249af9b
Reuben Agbor <reubenagbor10@gmail.com>
2026-03-26 17:49:39 +0100

    update

--- apps/web/components/shop/product-card.tsx
+++ apps/web/components/shop/product-card.tsx
@@ -3,8 +3,6 @@ import React from 'react'
 import { Button } from '../ui/button'
 import Image from 'next/image'
 import Link from 'next/link'
-import Image from 'next/image'
-import Link from 'next/link'
 
 interface Product {
   id: number
